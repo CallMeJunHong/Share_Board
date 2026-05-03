d3a31ed393de86335738d8b856e4e74b1e32eda2
JunHong-Main <junhong051219@gmail.com>
2026-05-03 14:03:01 +0800

    Refactor axios and socket configurations to use environment variables for base URLs

--- client/src/lib/axios-instance.js
+++ client/src/lib/axios-instance.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Create an Axios instance with default configuration
 // Cleaner code, create seperate configurations for different APIS without repeating settings.
+const base = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4200'; // Use environment variable for base URL or fallback to localhost
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4200',
+    baseURL: base,
     headers: {
         'Content-Type': 'application/json', // Set default content type and as we are using JSON
     }
--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4200', {
+const backendUrl = import.meta.env.VITE_API_BASE || 'http://localhost:4200'; // Use environment variable for backend URL or fallback to localhost
+const socket = io(backendUrl, {
     autoConnect: false, 
     reconnectionDelayMax: 10000,
 });
