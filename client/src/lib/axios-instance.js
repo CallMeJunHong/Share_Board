84885cacf0851d4adf98914ba603709bd710e873
JunHong-Main <junhong051219@gmail.com>
2026-05-03 14:10:05 +0800

    Fix environment variable reference for API base URL in axios instance

--- client/src/lib/axios-instance.js
+++ client/src/lib/axios-instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default configuration
 // Cleaner code, create seperate configurations for different APIS without repeating settings.
-const base = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4200'; // Use environment variable for base URL or fallback to localhost
+const base = import.meta.env.VITE_API_BASE || 'http://localhost:4200'; // Use environment variable for base URL or fallback to localhost
 export const axiosInstance = axios.create({
     baseURL: base,
     headers: {
